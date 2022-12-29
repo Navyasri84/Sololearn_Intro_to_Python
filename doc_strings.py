71cc2649a008e4104bbbd3ea9bb44ca3cfddf9bc
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2022-12-29 16:18:33 +0530

    Add files via upload

---
+++ doc_strings.py
@@ -0,0 +1,13 @@
+'''Docsstrings
+Docstrings (documentation strings) are similar to comments, in that they’re designed 
+to explain code. But, they’re more specific and have a different syntax.
+They’re created by putting a multiline string containing an explanation of the function 
+below the function's first line'''
+
+# def shout(word):
+#     """
+#     Print a word with an
+#     exclamation mark following it.
+#     """
+#     print(word + "!")
+# shout("spam")
