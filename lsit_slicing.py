0f49e91f2c183e6f4728916e303209fe714b5526
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2023-01-01 19:17:50 +0530

    Add files via upload

---
+++ lsit_slicing.py
@@ -0,0 +1,34 @@
+# squares=[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(squares[2:6])
+# print(squares[3:8])
+# print(squares[0:1])
+
+# squares=[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(squares[:7])
+
+# squares=[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(squares[7:])
+
+# squares=[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(squares[::2])
+# print(squares[2:8:3])
+
+'''Negative values can also be used in list slicing (as well as normal list indexing). 
+Which means that when negative values are used for the first and second values in a slice 
+(or a normal index), they count from the end of the list. '''
+
+# squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(squares[1:-1])
+
+# sqs=[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(sqs[7:5:-1])
+
+# nums=[5, 42, 7, 1, 0]
+# res=nums[::-1]
+# print(res)
+"""Write a program that takes a string as input and outputs the last character of that 
+string."""
+
+# str=input()
+# print(str[-1])
+
